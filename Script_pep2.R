48fa6f436acbbc00eefd9e71ee886fa6402ce038
0carloscastillo0 <carlos.castillo.d@usach.cl>
2022-01-17 17:33:02 -0300

    retroalimentacion script

--- Script_pep2.R
+++ Script_pep2.R
@@ -1636,3 +1636,97 @@ cat (" - - - - - - - - - - -- - - - - -\n")
 print ( entrenamiento [ sospechosos , ])
 cat ("\n\n")
 print ( output [ sospechosos , ])
+
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+library ( ggpubr )
+library ( tidyr )
+#-------------------------------------------------------------------------------
+#                                 Pregunta 1
+#-------------------------------------------------------------------------------
+# Dos artículos reportan el porcentaje de acierto alcanzado por dos algoritmos 
+# de clasificaciïón, específicamente el Bayes ingenuo (C4) y el Bayes ingenuo 
+# oculto (C2), en diferentes conjuntos de prueba disponibles en el UCI Machine 
+# Learning Repository. ¿Es uno de los algoritmo mejor que el otro?
+
+texto <-("
+ Dataset C2 Dataset C4
+ 'tae' 43.82 'credit' 85.67
+ 'anneal' 97.44 'monks' 61.68
+ 'pasture-production' 85.27 'soybean' 91.52
+ 'contact-lenses' 67.77 'segment' 90.74
+ 'primary-tumor' 47.52 'squash-unstored' 61.11
+ 'kr-s-kp' 91.90 'mushroom' 95.27
+ 'solar-flare-C' 87.68 'page-blocks' 92.95
+ 'monks1' 99.44 'grub-damage' 47.23
+ 'white-clover' 78.73 'cmc' 50.49
+ 'ecoli' 79.48 'waveform' 79.30
+ 'nursery' 93.72 'postoperatie' 66.11
+ 'squash-stored' 57.44 -- --
+")
+datos <- read.table(textConnection(texto), header = TRUE, na.strings = "--")
+#-------------------------------------------------------------------------------
+# Respuesta:  
+cat("\n----------------------------------------------------------------------")
+cat("\n                            PREGUNTA 1") 
+cat("\n----------------------------------------------------------------------\n")
+
+# Para responder dicha pregunta, se debe diferenciar los datos entregados.
+
+# En primera instancia, se puede observar que los datos son independientes,  
+# cumpliendo la condición de independencia para poder usar la prueba t-student.
+# Pero no se puede usar dicha prueba, debido a dos razones.
+# - El primero es que no se podrá asumir que la escala que presenta los datos 
+#   sean de intervalos iguales.
+# - Y el segundo es que las distribución de los datos no se asemejan a la normal,
+#   en la cual se puede verificarse en los siguientes histogramas:
+
+# Histograma del algoritmo Bayes ingenuo oculto (C2)
+g1 <- gghistogram ( datos ,
+                    x = "C2",
+                    bins = 10,
+                    add = "mean",
+                    xlab = "Porcentaje de acierto",
+                    ylab = "Frecuencia",
+                    title = "Histograma del algoritmo C2",
+                    color = "black",
+                    fill = "blue")
+print(g1)
+
+# Histograma del algoritmo Bayes ingenuo (C4)
+g2 <- gghistogram ( datos ,
+                    x = "C4",
+                    bins = 10,
+                    add = "mean",
+                    xlab = "Porcentaje de acierto",
+                    ylab = "Frecuencia",
+                    title = "Histograma del algoritmo C4",
+                    color = "black",
+                    fill = "blue")
+print(g2)
+
+# Debido a lo anterior, la única opción que queda es en usar la prueba no paramétrica 
+# de Wilcoxon-Mann-Whitney, considerando las siguientes hipótesis:
+
+# H0: No hay diferencia en el porcentaje de acierto en ambos algoritmos.
+# HA: Si hay diferencia en el porcentaje de acierto en ambos algoritmos.
+
+# Entonces, con un nivel de significación del 1%, se hará la prueba mediante la
+# función wilcox.test de R:
+
+alfa <- 0.01
+prueba <- wilcox.test(datos$C2 , 
+                      datos$C4 , 
+                      alternative = "two.sided", 
+                      conf.level = 1 - alfa )
+cat("\nPrueba de suma de rangos de Wilcoxon mediante wilcox.test:\n")
+print ( prueba )
+
+# Entonces, de acuerdo a los resultados, el valor de p obtenido es 0.6947, 
+# siendo demasiado alto para el nivel de significancia definido (alfa = 0.01),
+# por lo que se falla al rechazar la hipótesis nula a favor de la alternativa.
+# Por lo tanto, con un 99% de confianza, se concluye que no existe diferencia 
+# en el porcentaje de acierto en ambos algoritmos, por lo que ningún algoritmo
+# es mejor que otro.
